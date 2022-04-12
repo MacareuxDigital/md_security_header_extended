cc8ee8710e523c4b074e410043df619167edf79d
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-04-13 03:01:23 +0900

    Release v1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Release Notes
+
+## [v1.1 (April 13, 2022)]
+
+### Fixes
+
+- Fix some bugs on PHP 5.6
+- Fix some bugs on Concrete <8.5.3
+
+## [v1.0 (March 28, 2022)]
+
+Initial Release
--- controller.php
+++ controller.php
@@ -30,7 +30,7 @@ class Controller extends Package
      *
      * @var string
      */
-    protected $pkgVersion = '0.0.2';
+    protected $pkgVersion = '1.1.0';
 
     /**
      * @see https://documentation.concretecms.org/developers/packages/adding-custom-code-to-packages
